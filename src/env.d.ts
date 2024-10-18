d7b58bbdd66cbb0468d54991d335b9890369df8b
Miguel Bonilla <miguel@viguedevelop.com>
2024-10-18 11:41:01 -0600

    appwrite init

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_APPWRITE_ENDPOINT: string;
+  readonly PUBLIC_APPWRITE_PROJECT_ID: string;
+  readonly PUBLIC_APPWRITE_DB_ID: string;
+  readonly PUBLIC_APPWRITE_POSTS_ID: string;
+}
---
+++ src/lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, Databases, Storage } from 'appwrite';
+
+export const appwriteClient = new Client()
+  .setEndpoint(import.meta.env.PUBLIC_APPWRITE_ENDPOINT) // URL de tu servidor Appwrite
+  .setProject(import.meta.env.PUBLIC_APPWRITE_PROJECT_ID); // ID de tu proyecto
+
+export const appwriteDatabases = new Databases(appwriteClient);
+export const appwriteStorage = new Storage(appwriteClient);
+
+
+
