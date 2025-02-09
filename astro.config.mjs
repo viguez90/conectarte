167641d3bde4548d097ac5dade901a4f0e997283
Miguel Bonilla <miguel@viguedevelop.com>
2025-02-08 22:44:32 -0600

    viva la vida 2.0.4: node

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  output: 'hybrid',
+  output: 'server',
 
   adapter: node({
     mode: 'standalone',
