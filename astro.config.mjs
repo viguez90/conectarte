f2305bfffe3ce0407c3821b0d391bd589753a9be
Miguel Bonilla <miguel@viguedevelop.com>
2025-02-08 22:24:31 -0600

    viva la vida 2.0.1: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  output: 'server'
+  output: 'hybrid',
+  adapter: netlify()
 });
