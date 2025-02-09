e59f9a27ef63e7c1213b46b04cc0440dfc45936c
Miguel Bonilla <miguel@viguedevelop.com>
2025-02-09 00:35:38 -0600

    viva la vida 2.2.2: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   integrations: [svelte()],
   output: 'server',
+  adapter: vercel(),
 });
