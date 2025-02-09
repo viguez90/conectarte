575f6ee403094b1424543f2921d3efb74a5a3da1
Miguel Bonilla <miguel@viguedevelop.com>
2025-02-08 23:41:04 -0600

    viva la vida 2.1.2: reestructure rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  output: 'server',
+  output: 'hybrid',
 
   adapter: node({
     mode: 'standalone',
